49ffd58533e8e0fe27eb5e1870be0d273defb5b9
Þorvaldur <breki121212@gmail.com>
2025-12-11 11:12:33 +0000

    simple update

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,13 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import favoritesReducer from "./favorites_slice";
 import upcomingReducer from "./upcoming_movies_slice";
 import movieDetailsReducer from "./upcoming_movie_details_slice";
+import moviesReducer from "./current_movie_slice";
 
 export const store = configureStore({
   reducer: {
     favorites: favoritesReducer,
     upcoming: upcomingReducer,
     movieDetails: movieDetailsReducer,
-    // add more slices here as you build them
+    movies:moviesReducer,
   },
 });
 
